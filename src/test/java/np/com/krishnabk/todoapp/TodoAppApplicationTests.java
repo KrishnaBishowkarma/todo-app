0d0074f2b7e5ada4e8c98feda23f3e3487c0107d
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2025-06-10 15:13:01 +0545

    initial commit - todo app

---
+++ src/test/java/np/com/krishnabk/todoapp/TodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package np.com.krishnabk.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
